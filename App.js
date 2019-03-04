9363b98b368d8ee0d16483bacfc7b0cf0e719c6b
melih sahin <melihsahin24@gmail.com>
2019-03-04 11:22:50 +0300

    App.js updated

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-/**
+ /**
  * Sample React Native App
  * https://github.com/facebook/react-native
  *
@@ -8,7 +8,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View,ScrollView} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,30 +21,18 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={{flex : 1,marginTop:Platform.OS == 'ios' ? 21 : 0}}>
+      <View style={{backgroundColor: 'yellow',height: 100}}>
+        <View style={{backgroundColor :'blue', flex:4}}>
+        </View>
+        <View style={{backgroundColor :'red', flex:1}}>
+        </View>
+      </View>
+        <ScrollView>
+
+        </ScrollView>
+
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
